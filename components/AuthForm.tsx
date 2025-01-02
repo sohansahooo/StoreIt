9d9154293c5fa1a706a4d9e050ec3b052c7973e2
sohanxsahoo <116956354+sohanxsahoo@users.noreply.github.com>
2025-01-02 22:54:15 +0530

    Add utility functions, authentication pages, and SVG icons; update ESLint configuration

---
+++ components/AuthForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+type FormType = "sign-in" | "sign-up";
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const [isLoading, setIsLoading] = useState(initialState: false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  };
+
+  return (
+    <>
+      {/* // 3. Use your form. */}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                <FormLabel>Full Name</FormLabel>
+
+                <FormControl className="shad-form-control">
+                  <Input placeholder="Enter your full name" className="shad-input" {...field} />
+                </FormControl>
+
+                </div>
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />)}
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                <FormLabel>Email</FormLabel>
+
+                <FormControl className="shad-form-control">
+                  <Input placeholder="Enter your email" className="shad-input" {...field} />
+                </FormControl>
+
+                </div>
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="form-submit-button">{type === "sign-in" ? "Sign In" : "Sign Up"}</Button>
+        </form>
+      </Form>
+      {/* OTP VERIFICATION */}
+    </>
+  );
+};
+
+export default AuthForm;
